4a3abeece35198c33561f1f3437a2fb1f2397eaf
HansSima <SimaHonza@seznam.cz>
2025-06-03 20:39:18 +0200

    Pridan RodinnyDum

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //Random nahCislo = new Random();
+
+        RodinnyDum dumNovaku = new RodinnyDum();
+        dumNovaku.pocetPoschodi = 4;
+        dumNovaku.rozloha = 457f;
+        dumNovaku.material = "mramor";
+
+        RodinnyDum dumZajicu = new RodinnyDum();
+        dumZajicu.rozloha = 6468f;
+        dumZajicu.pocetPoschodi = 3;
+
+
+
 
     }
 }
